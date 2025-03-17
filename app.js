6c27f5c67587e8894ed4aef5926116ef7dd397d7
Arjun Santhosh <arjunsanthosh738@gmail.com>
2025-03-17 14:50:46 +0530

    major changes phase 1

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(express.urlencoded({ extended: true }));
 
 console.log(path.join(__dirname, 'public/uploads'));
 // Serve static files from public directory
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+app.use('/uploads', express.static('/public/uploads'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Routes
